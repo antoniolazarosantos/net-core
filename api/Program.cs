1296d5bf748c8b2400a5b97ec88c9d7204d0bdd5
antoniolazarosantos <antoniolazarosantos@gmail.com>
2022-07-07 18:35:52 -0300

    Refatorando apis-2

--- api/Program.cs
+++ api/Program.cs
@@ -59,6 +59,7 @@ public static class ProdutoRepositorio{
         ListaProdutos.Remove(produto);
     }
 }
+
 public class Produto {
     public string Codigo { get; set; }
     public string Nome { get; set; }
